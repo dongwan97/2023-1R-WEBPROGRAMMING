a20f0e7d4a10944cad08c0b0ba8d4e0386d07bdf
dongwan97 <ehddhks0448@naver.com>
2023-05-05 18:10:25 +0900

    9주차 과제

--- 2023-1R-WEBPROGRAMMING/src/App.jsx
+++ 2023-1R-WEBPROGRAMMING/src/App.jsx
@@ -1,26 +1,64 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
+// assets/react.svg에서 reactLogo를 가져온다.
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [row, setRow] = useState([]);
+
+  function handleButtonClick() {
+    if (row.length === 0) {
+      fetch(
+        "http://openapi.seoul.go.kr:8088/6c5462437265686439375a52657474/json/RealtimeCityAir/1/25/"
+      ).then(function (res2) {
+        res2.json().then(function (res3) {
+          setRow(res3.RealtimeCityAir.row);
+        });
+      });
+    }
+  }
+
+  console.log(row);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + wandong</h1>
+      <button onClick={handleButtonClick}>로딩중</button>
+      {row.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              <th>이름</th>
+              <th>PM10</th>
+              <th>O3</th>
+              <th>상태</th>
+            </tr>
+          </thead>
+          <tbody>
+            {row.map(function (obj, index) {
+              return (
+                <tr key={index}>
+                  <td>{obj.MSRSTE_NM}</td>
+                  <td>{obj.PM10}</td>
+                  <td>{obj.O3}</td>
+                  <td>{obj.IDEX_NM}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
+
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
--- 2023-1R-WEBPROGRAMMING/src/main.jsx
+++ 2023-1R-WEBPROGRAMMING/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+// App.jsx라는 파일에서 App을 가져온다.
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// ReactDOM.createRoot는 리액트를 실행하는 함수입니다.
+// .render는 App을 실행하는 함수입니다.
+// root라는 태그를 찾아서 App을 넣어주세요.
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
